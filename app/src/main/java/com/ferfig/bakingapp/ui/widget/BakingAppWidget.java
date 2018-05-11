ec1690ba79819f193c2c62925246e7563eddc917
FerFig <ferfig.apps@gmail.com>
2018-05-12 00:51:10 +0100

    Add widget (First implementation)

---
+++ app/src/main/java/com/ferfig/bakingapp/ui/widget/BakingAppWidget.java
@@ -0,0 +1,53 @@
+package com.ferfig.bakingapp.ui.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.ferfig.bakingapp.R;
+import com.ferfig.bakingapp.ui.MainActivity;
+
+public class BakingAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_app_widget);
+
+        views.setTextViewText(R.id.tv_widget_recipe_name, "receita");
+        views.setTextViewText(R.id.tv_widget_recipe_ingredients, "ingredientes\n · Asdkf asd ja\n · Baskljfs\n · Dsdkfj sad  asdak");
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        views.setOnClickPendingIntent(R.id.widget_screen, pendingIntent);
+        //views.setOnClickPendingIntent(R.id.tv_widget_recipe_name, pendingIntent);
+        //views.setOnClickPendingIntent(R.id.tv_widget_recipe_ingredients, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
